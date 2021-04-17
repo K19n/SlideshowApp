7fbd6a2e70e99777de6121251a39feceb5cd6c7c
keinun <yasumasa.kei@gmail.com>
2021-04-17 16:38:22 +0900

    画像遷移の部分修正しました。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -14,14 +14,14 @@ class ViewController: UIViewController {
     @IBOutlet weak var backButton: UIButton!
     
     var nowIndex:Int = 0
-
+    
     var timer: Timer!
-
+    
     var imageArray:[UIImage] = [
-            UIImage(named: "1")!,
-            UIImage(named: "2")!,
-            UIImage(named: "3")!
-        ]
+        UIImage(named: "1")!,
+        UIImage(named: "2")!,
+        UIImage(named: "3")!
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,33 +30,45 @@ class ViewController: UIViewController {
     }
     override func prepare(for segue:UIStoryboardSegue,sender:Any?){
         let nextViewController:NextViewController = segue.destination as! NextViewController
-
+        
+        if (timer == nil){
             nextViewController.image = imageView.image
+            startButton.setTitle("再生", for: .normal)
+            goButton.isEnabled = true
+            backButton.isEnabled = true
+        }else{
+            nextViewController.image = imageView.image
+            timer.invalidate()
+            timer = nil
+            startButton.setTitle("再生", for: .normal)
+            goButton.isEnabled = true
+            backButton.isEnabled = true
+        }
     }
-
+    
     @IBAction func slideShowButton(_ sender: Any) {
-               if (timer == nil) {
-                   timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
-
-                   startButton.setTitle("停止", for: .normal)
-                goButton.isEnabled = false
-                backButton.isEnabled = false
-               
-               } else {
-                   timer.invalidate()
-                   timer = nil
-                   startButton.setTitle("再生", for: .normal)
-                goButton.isEnabled = true
-                backButton.isEnabled = true
-               }
+        if (timer == nil) {
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
+            
+            startButton.setTitle("停止", for: .normal)
+            goButton.isEnabled = false
+            backButton.isEnabled = false
+            
+        } else {
+            timer.invalidate()
+            timer = nil
+            startButton.setTitle("再生", for: .normal)
+            goButton.isEnabled = true
+            backButton.isEnabled = true
+        }
     }    
-
+    
     @IBAction func changeImage(_ sender: Any) {
         nowIndex += 1
-            if (nowIndex == imageArray.count) {
-                nowIndex = 0
-            }
-            imageView.image = imageArray[nowIndex]
+        if (nowIndex == imageArray.count) {
+            nowIndex = 0
+        }
+        imageView.image = imageArray[nowIndex]
     }
     
     @IBAction func backImage(_ sender: Any) {
